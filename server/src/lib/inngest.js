da08c0d5b434688218a94b2b1f439b9ba35f705f
swasthikg73 <swasthik@folkslogic.com>
2026-06-03 15:57:22 +0530

    Stream configs

--- server/src/lib/inngest.js
+++ server/src/lib/inngest.js
@@ -1,6 +1,7 @@
 import { Inngest } from "inngest";
 import ConnectDb from "./db.js";
 import User from "../models/User.js";
+import { deleteStreamUser, upsertStreamUser } from "./stream.js";
 // Create a client to send and receive events
 export const inngest = new Inngest({ id: "Codexa" });
 
@@ -24,6 +25,12 @@ const syncUser = inngest.createFunction(
       profileImage: image_url,
     };
     await User.create(newUser);
+
+    await upsertStreamUser({
+      id: newUser.clerkId.toString(),
+      name: newUser.name,
+      image: newUser.profileImage,
+    });
   },
 );
 
@@ -33,8 +40,9 @@ const deleteUserFromDB = inngest.createFunction(
     await ConnectDb();
 
     const { id } = event.data;
-
     await User.deleteOne({ clerkId: id });
+
+    await deleteStreamUser(id.toString());
   },
 );
 
