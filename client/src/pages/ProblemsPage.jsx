4dbe3422eeba4a260f452ad0dfcdaa5b2c2acebd
swasthikg73 <swasthik@folkslogic.com>
2026-06-12 15:24:53 +0530

    Problem details page route updated

--- client/src/pages/ProblemsPage.jsx
+++ client/src/pages/ProblemsPage.jsx
@@ -38,7 +38,7 @@ function ProblemsPage() {
           {problems.map((problem) => (
             <Link
               key={problem.id}
-              to={`problem/${problem.id}`}
+              to={`/problem/${problem.id}`}
               className="card bg-base-100 hover:scale-[1.01] transition-transform">
               <div className="card-body">
                 <div className="flex items-center justify-center gap-4">
